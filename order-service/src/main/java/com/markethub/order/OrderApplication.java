807503008a0dfa45f1c754315149d505f223b64f
Vagner Ventura <vventura.ekan@iamspe.sp.gov.br>
2026-04-21 22:02:12 -0300

    chore(order): add application bootstrap
    - adiciona classe principal

--- order-service/src/main/java/com/markethub/order/OrderApplication.java
+++ order-service/src/main/java/com/markethub/order/OrderApplication.java
@@ -1,4 +1,15 @@
 package com.markethub.order;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableKafka
 public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
 }
